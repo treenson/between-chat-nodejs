67546b6d11a83409fc3851e05876cda77bbcfae1
Treenson <treenson@gmail.com>
2013-05-17 09:43:30 +0000

    Saving Chatting Log

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var io = require('socket.io').listen(8080);
 var log = [];
 
+var log4js = require('log4js');
+log4js.clearAppenders()
+log4js.loadAppender('file');
+log4js.addAppender(log4js.appenders.file('chat-content.log'), 'chat');
+var logger = log4js.getLogger('chat');
+logger.setLevel('info');
+
 io.sockets.on('connection',function(socket) {
 	socket.emit('news', {old: log});
 	socket.on('ping', function(data){
@@ -15,8 +22,11 @@ io.sockets.on('connection',function(socket) {
 		{
 			var send_str=user+data['content'];
 			io.sockets.emit('pong', {content: send_str});
+
+			logger.info(send_str);
+
 			log.push(send_str);
-			if(log.length > 50)
+			if(log.length > 100)
 				log.shift();
 		}
 	});
